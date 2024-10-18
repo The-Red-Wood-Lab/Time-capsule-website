78610f2d7eb5e1e7b4d7a1106a6b884845924447
blackie360 <felixkent360@gmail.com>
2024-10-18 15:06:54 +0300

    feat:auth

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,19 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { ClerkProvider } from '@clerk/clerk-react'
+
+// Import your publishable key
+const PUBLISHABLE_KEY = "pk_test_cmVsYXhpbmctdGFwaXItMTEuY2xlcmsuYWNjb3VudHMuZGV2JA"
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <App />
+    </ClerkProvider>
   </StrictMode>,
 )
