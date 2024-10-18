db99d4a390eaf6aeb55cd8fd37f2acf2393c9d68
blackie360 <felixkent360@gmail.com>
2024-10-18 16:12:50 +0300

    cretated a landing page, nav bar and user can sin in and out of thier accounts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,33 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import TimeCapsule from "./components/TimeCapsule";
-import './index.css'
+import './index.css';
+import Hero from "./components/Hero";
+import NavBar from "./components/NavBar"; // Import NavBar
+import { SignedIn } from "@clerk/clerk-react";
+
+function ProtectedRoute({ children }) {
+  return (
+    <SignedIn>
+      {children}
+    </SignedIn>
+  );
+}
 
 export default function App() {
   return (
-    <header>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      
-    </header>
+    <Router>
+      <NavBar /> 
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route
+          path="/timecapsule"
+          element={
+            <ProtectedRoute>
+              <TimeCapsule />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
