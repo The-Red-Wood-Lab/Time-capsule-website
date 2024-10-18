9323a3ab26107b17a39e722d51e10dee9048f22a
blackie360 <felixkent360@gmail.com>
2024-10-18 16:57:22 +0300

    removed env
    This reverts commit 6d60ed66e586d41bfe6d4a26651e014e2970ba22.

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,13 +1,10 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
-import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <nav className="bg-gray-900 shadow-md">
       <div className="max-w-6xl mx-auto px-4 py-3 flex justify-between items-center">
-        <h1 className="text-2xl font-bold text-blue-600">
-          <Link to="/">
-          Time Capsule </Link></h1>
+        <h1 className="text-2xl font-bold text-blue-600">Time Capsule</h1>
         <div className="flex items-center space-x-4">
           <SignedOut>
             <SignInButton className="bg-blue-600 text-black rounded-lg py-2 px-4 hover:bg-blue-700 transition duration-300">
